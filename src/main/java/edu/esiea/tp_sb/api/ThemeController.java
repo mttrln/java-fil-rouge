88c757d96423c17307a4e1ce23a3374396c01ba5
mttrln <256447621+mttrln@users.noreply.github.com>
2026-01-22 14:22:56 +0100

    PreAuthorize

--- src/main/java/edu/esiea/tp_sb/api/ThemeController.java
+++ src/main/java/edu/esiea/tp_sb/api/ThemeController.java
@@ -7,6 +7,7 @@ import edu.esiea.tp_sb.dto.lesson.LessonPostDto;
 import edu.esiea.tp_sb.dto.theme.ThemeDto;
 import edu.esiea.tp_sb.dto.theme.ThemePostDto;
 import jakarta.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -37,6 +38,7 @@ public class ThemeController {
     }
 
     @PostMapping("/{id}/lessons")
+    @PreAuthorize("hasAnyRole('AUTHOR', 'ADMIN')")
     public LessonDto createLesson(@Valid @RequestBody LessonPostDto lessonDto, @PathVariable long id)
     {
         return themeService.createLesson(lessonDto, id);
