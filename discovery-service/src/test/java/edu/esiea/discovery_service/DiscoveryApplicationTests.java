e90ab701e96ecd1d6083113e1d1a0e4811363b15
Benjamin CHARNET <benjamin.charnet.pro@gmail.com>
2026-01-29 12:08:09 +0100

    :sparkles: Begin 2

---
+++ discovery-service/src/test/java/edu/esiea/discovery_service/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.esiea.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
